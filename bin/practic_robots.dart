040ea3458de7d1d95b4eccb80763a848d458e2e1
JuanCaceffo <caceffojuan@gmail.com>
2023-03-11 22:11:03 -0300

    initial commit

---
+++ bin/practic_robots.dart
@@ -0,0 +1,5 @@
+import 'package:practic_robots/practic_robots.dart' as practic_robots;
+
+void main(List<String> arguments) {
+  print('Hello world: ${practic_robots.calculate()}!');
+}
---
+++ test/practic_robots_test.dart
@@ -0,0 +1,8 @@
+import 'package:practic_robots/practic_robots.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
